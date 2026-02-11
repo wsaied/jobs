9a35c1a0d9146dc09f2be5b25dc3afba42b6aed5
wsaied <waleeedsaied@gmail.com>
2026-02-11 14:47:13 +0200

    Create basic webpack configuration file for Node.js application

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    target: 'node',
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                exclude: /node_modules/, 
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+};
